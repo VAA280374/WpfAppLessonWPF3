56f5dd38b351aea99c68dde7c12ec9aeaf21ef19
alexander.a.volkov@rambler.ru <alexander.a.volkov@rambler.ru>
2022-01-26 21:56:24 +0300

    пока без цветов

--- WpfAppLessonWPF3/MainWindow.xaml.cs
+++ WpfAppLessonWPF3/MainWindow.xaml.cs
@@ -16,13 +16,63 @@ using System.Windows.Shapes;
 namespace WpfAppLessonWPF3
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml   
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            if (textBox != null)
+            {
+                textBox.FontFamily = new FontFamily (fontName);
+            }
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            double fontSise = Convert.ToDouble(((sender as ComboBox).SelectedItem as TextBlock).Text);
+            if (textBox != null)
+            {
+                textBox.FontSize = fontSise;
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                if (textBox.FontWeight != FontWeights.Bold)
+                    textBox.FontWeight = FontWeights.Bold;
+                else textBox.FontWeight = FontWeights.Normal;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                if (textBox.FontStyle != FontStyles.Italic)
+                    textBox.FontStyle = FontStyles.Italic;
+                else textBox.FontStyle = FontStyles.Normal;
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                if (textBox.TextDecorations != TextDecorations.Underline)
+                    textBox.TextDecorations = TextDecorations.Underline;
+                else textBox.TextDecorations = null;
+            }
+
         }
     }
 }
